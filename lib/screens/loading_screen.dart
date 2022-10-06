e15bb62fb2d6a23df00cca581f8c46d3722db41e
Awesomewsdm <88857387+Awesomewsdm@users.noreply.github.com>
2022-10-06 22:16:50 +0100

    getting access to OpenWeather's API

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,19 +1,46 @@
- import 'package:flutter/material.dart';
+import 'package:clima/screens/location.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+
 
 class LoadingScreen extends StatefulWidget {
   const LoadingScreen({super.key});
 
   @override
-  _LoadingScreenState createState() => _LoadingScreenState();
+  LoadingScreenState createState() => LoadingScreenState();
 }
 
-class _LoadingScreenState extends State<LoadingScreen> {
+class LoadingScreenState extends State<LoadingScreen> {
+  @override
+  void initState() {
+    getLocation();
+    super.initState();
+  }
+
+  void getLocation() async {
+    Location newLocation = Location();
+    await newLocation.getCurrentLocation();
+    // ignore: avoid_print
+    print(newLocation.latitude);
+    // ignore: avoid_print
+    print(newLocation.longitude);
+  }
+
+  void getData () async{
+    var url =
+      Uri.https('www.googleapis.com', '/books/v1/volumes', {'q': '{http}'});
+    // ignore: unused_local_variable
+    http.Response response = await  http.get(url);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: ElevatedButton(
           onPressed: () {
+            getLocation();
             //Get the current location
           },
           child: const Text('Get Location'),
